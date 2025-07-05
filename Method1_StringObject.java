fe40ca351f53c3161c7e757bdd5c0862d81d018b
emmanuelotech <150592072+emmanuelotech@users.noreply.github.com>
2025-07-05 15:26:07 +0300

    QUESTION 2

---
+++ Method1_StringObject.java
@@ -0,0 +1,10 @@
+package question_2b;
+
+public class Method1_StringObject {
+    private String title = "";  
+    private String author = "";
+
+    public Method1_StringObject() {
+        
+    }
+}
---
+++ Method2_StringObjectCreation.java
@@ -0,0 +1,16 @@
+
+package question_2b;
+
+public class Method2_StringObjectCreation {
+    
+    
+    
+    private String title = new String();  // Explicit empty String object
+    private String author = new String();
+
+    public Method2_StringObjectCreation() {
+        System.out.println("Our book string declarion.knkjlje hereJ");
+  
+}
+    
+}
